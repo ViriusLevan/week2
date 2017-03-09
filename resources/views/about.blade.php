605e16f1c86f34833f099f8897ebfa7d6d23f782
ViriusLevan <Mantor562@gmail.com>
2017-03-09 14:36:00 +0700

    I know you're reading this

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,4 +1,6 @@
-<a>We're a happy family </a><br>
-
+<h1>We're a happy family </h1><br>
+<h2>Sit down and have some dinner now </h2><br>
+<h3>Ya better behave now </h3><br>
+<h4>Or i'll have to discipline ya</h4><br>
 
 <a href ="{{url('home')}}">Go home</a><br>
